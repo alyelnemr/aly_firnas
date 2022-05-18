765104490e154635b4c828d21cdf77ba27df1892
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-05-18 13:17:04 +0200

    [fix] analytic tag and account not required in account payment to allow reconcile screen work

--- bi_analytic_accounts_analytic_tags/models/inherit_account_payment.py
+++ bi_analytic_accounts_analytic_tags/models/inherit_account_payment.py
@@ -7,8 +7,8 @@ class InheritAccountPayment(models.Model):
     _inherit = 'account.payment'
 
     analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', index=True,
-                                          required=True)
-    analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags', required=True)
+                                          required=False)
+    analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags', required=False)
 
     def _prepare_payment_moves(self):
         res = super(InheritAccountPayment, self)._prepare_payment_moves()
