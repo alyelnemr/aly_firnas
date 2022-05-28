b8c22eae0f639bab19c1f1a165e3f14ed9bf6aae
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-05-28 03:32:14 +0200

    [add] encrypt mail password2

--- aly_firnas/models/ir_mail_server.py
+++ aly_firnas/models/ir_mail_server.py
@@ -139,12 +139,12 @@ class IrMailServer(models.Model):
     def write(self, vals):
         for rec in self:
             if vals.get('smtp_pass') and not rec.is_password_encrypted:
-                publicKey, privateKey = rsa.newkeys(150)
+                publicKey, privateKey = rsa.newkeys(512)
                 smtp_pass = vals.get('smtp_pass')
                 encMessage = rsa.encrypt(smtp_pass.encode(),
                                          publicKey)
                 vals.update({
-                    'smtp_pass': encMessage,
+                    'smtp_pass': str(encMessage),
                     'pass_public_key': publicKey,
                     'pass_private_key': privateKey,
                     'is_password_encrypted': True,
