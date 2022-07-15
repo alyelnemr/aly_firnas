6d7ea9409a6a90f848812a0bcae93c74b3a831d5
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-07-15 03:16:59 +0200

    [add] allow to reset to draft for expense report even it was aleady posted, then journal entry will be cancelled

--- aly_firnas/models/hr_expense_sheet.py
+++ aly_firnas/models/hr_expense_sheet.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import email_split, float_is_zero
 
 
 class HrExpenseSheet(models.Model):
@@ -13,3 +15,14 @@ class HrExpenseSheet(models.Model):
         self.address_id = self.employee_id.sudo().address_home_id
         self.department_id = self.employee_id.department_id
         self.user_id = False
+
+    def reset_expense_sheets(self):
+        if not self.can_reset:
+            raise UserError(_("Only HR Officers or the concerned employee can reset to draft."))
+        self.mapped('expense_line_ids').write({'is_refused': False})
+        move = self.env['account.move'].browse(self.account_move_id.id)
+        if move:
+            move.write({'state': 'cancel'})
+        self.write({'state': 'draft', 'account_move_id': False})
+        self.activity_update()
+        return True
