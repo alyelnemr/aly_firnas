dc815fa103a6e869f4b213f53ee0bdc2a9958001
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-08-16 18:33:16 +0200

    [upgrade] bi_crm_customization, bi_crm_concatenated_name

--- aly_firnas/__manifest__.py
+++ aly_firnas/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': "Aly El Nemr, Odoo Development for Firnas Shuman",
     'description': """Aly El Nemr, aly5elnemr@gmail.com, Odoo Development for Firnas Shuman""",
     'author': "Aly El Nemr",
-    'depends': ['purchase', 'sale', 'sale_management', 'account', 'bi_product_bundle_report'],
+    'depends': ['purchase', 'sale', 'sale_management', 'account', 'bi_product_bundle_report', 'bi_crm_concatenated_name', 'bi_crm_customization'],
 
     'data': [
         'security/ir.model.access.csv',
