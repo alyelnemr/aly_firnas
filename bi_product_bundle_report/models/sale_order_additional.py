b9db0921e96822e77c8ffec0d9d29268bc1808ac
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-10-21 18:59:28 +0200

    [add] sales & crm comments 19-Oct 01

--- bi_product_bundle_report/models/sale_order_additional.py
+++ bi_product_bundle_report/models/sale_order_additional.py
@@ -95,9 +95,6 @@ class SaleOrderAdditional(models.Model):
         }
 
     def action_update_factor(self):
-        """
-        Compute the amounts of the SO line.
-        """
         for line in self:
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
             custom_rate = self.order_id.custom_rate
@@ -105,13 +102,3 @@ class SaleOrderAdditional(models.Model):
             if is_manual and custom_rate > 0:
                 custom_rate = self.order_id.custom_rate
                 line.price_unit *= custom_rate
-            taxes = line.tax_id.compute_all(line.price_unit, line.order_id.currency_id, line.product_uom_qty,
-                                            product=line.product_id,
-                                            partner=line.order_id.partner_shipping_id)
-            line.update({
-                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                'price_total': taxes['total_included'],
-                'price_subtotal': taxes['total_excluded'],
-            })
-            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
-                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
