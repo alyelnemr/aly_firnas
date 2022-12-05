e56c239254fcabcd1dbc7644b8e8431cc79e0065
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-12-05 12:10:05 +0200

    [fix] issue of sub total for bundle product while printing0

--- bi_product_bundle_report/models/sale_report.py
+++ bi_product_bundle_report/models/sale_report.py
@@ -17,7 +17,8 @@ class SalesOrderReport(models.AbstractModel):
         is_additional_exists = any(docs.sale_order_additional_ids.filtered(lambda o: not o.is_button_clicked))
         is_optional_exists = any(docs.sale_order_option_ids.filtered(lambda o: not o.is_button_clicked))
         amount_untaxed = sum([
-            (ol.price_subtotal if not ol.product_id.child_line else ol.price_unit * ol.product_uom_qty) for ol in
+            (ol.price_subtotal if not ol.product_id.child_line else ol.product_uom_qty * (
+                        ol.price_unit - (ol.price_unit * ol.discount / 100))) for ol in
             docs.order_line.filtered(lambda l: l.is_printed and (not l.parent_order_line or l.parent_order_line.is_printed))
         ])
 
