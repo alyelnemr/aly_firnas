c6fd1c1253ac4999bd4fd9fd0795ebc903e9fad7
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-12-24 01:22:08 +0200

    [add] development of video 29-Nov 03

--- aly_firnas/models/product_template.py
+++ aly_firnas/models/product_template.py
@@ -13,3 +13,15 @@ class ProductProduct(models.Model):
     #     'Sales Description', translate=True, copy=False,
     #     help="A description of the Product that you want to communicate to your customers. "
     #          "This description will be copied to every Sales Order, Delivery Order and Customer Invoice/Credit Note")
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        # TDE FIXME: should probably be copy_data
+        self.ensure_one()
+        if default is None:
+            default = {}
+        if 'default_code' not in default:
+            default['default_code'] = self.default_code
+        if 'description_sale' not in default:
+            default['description_sale'] = False
+        return super(ProductProduct, self).copy(default=default)
