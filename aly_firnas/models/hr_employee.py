64a68ffb24b5b7a72c3c048e518f8e2c7e25c85b
Aly Abdel Hamid <aly5elnemr@gmail.com>
2023-01-09 21:41:54 +0200

    [fix] issue of hr employee when no code

--- aly_firnas/models/hr_employee.py
+++ aly_firnas/models/hr_employee.py
@@ -20,7 +20,7 @@ class MyEmployee(models.Model):
         analytic_tag_exist = self.env['account.analytic.tag'].search([('name', '=', tag_name)])
         if not analytic_tag_exist:
             analytic_tag = self.env['account.analytic.tag'].create({'name': tag_name})
-            employee.analytic_tag_ids = [(4, analytic_tag.ids)]
+            employee.analytic_tag_ids = analytic_tag.ids
         return employee
 
     def write(self, vals):
