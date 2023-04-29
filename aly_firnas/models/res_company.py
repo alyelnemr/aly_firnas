7e99933da689f2c47115430ca05e246ad6cc7432
Aly El Nemr <aly5elnemr@gmail.com>
2023-04-29 23:28:38 +0300

    [add] comments video of 23-01-2023

--- aly_firnas/models/res_company.py
+++ aly_firnas/models/res_company.py
@@ -9,6 +9,6 @@ class ResCompany(models.Model):
         default_company_id = self.default_get(['company_id'])['company_id']
         return self.env['account.journal'].search([('type', 'in', ['cash', 'bank'])], limit=1)
 
-    aly_expense_bank_journal_id = fields.Many2one('account.journal', string='Expenses - Default Bank Journal',
-                                                  domain="[('company_id', '=', id), ('type', 'in', ['cash', 'bank'])]", check_company=True)
+    # aly_expense_bank_journal_id = fields.Many2one('account.journal', string='Expenses - Default Bank Journal',
+    #                                               domain="[('company_id', '=', id), ('type', 'in', ['cash', 'bank'])]", check_company=True)
     date_sub_groups_ids = fields.Many2many('res.groups', string='Notification Groups')
